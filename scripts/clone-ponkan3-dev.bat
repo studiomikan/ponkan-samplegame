34de7cfabee677411c8e0ba404e3b1b04afa124d
okayu <moka1988jp@gmail.com>
2019-09-20 19:13:33 +0900

    first

---
+++ scripts/clone-ponkan3-dev.bat
@@ -0,0 +1,7 @@
+@echo off
+cd /d %~dp0
+cd ../
+git clone git@github.com:okayumoka/ponkan3.git ponkan3
+cd ./ponkan3
+git checkout -b develop origin/develop
+@echo on
---
+++ scripts/update-ponkan.bat
@@ -0,0 +1,11 @@
+@echo off
+cd /d %~dp0
+cd ../ponkan3
+git checkout .
+git pull
+call npm install
+call npm run release
+cd ../
+copy ponkan3\dist\ponkan3.js public\ponkan3.js
+pause
+@echo on
