e3451336c16a0cdda3aa30c615c8a6ee29b176f0
okayu <moka1988jp@gmail.com>
2019-09-29 22:05:36 +0900

    バッチを変更

--- scripts/clone-ponkan3.bat
+++ scripts/clone-ponkan3.bat
@@ -3,5 +3,5 @@ cd /d %~dp0
 cd ../
 git clone git@github.com:okayumoka/ponkan3.git ponkan3
 cd ./ponkan3
-git checkout -b develop origin/develop
+REM git checkout -b develop origin/develop
 @echo on
