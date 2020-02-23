48a66506233e8f559480b7077f49b70a2cccaf5f
okayu <moka1988jp@gmail.com>
2020-02-24 02:09:10 +0900

    fix repos url

--- scripts/clone-ponkan.bat
+++ scripts/clone-ponkan.bat
@@ -1,7 +1,7 @@
 @echo off
 cd /d %~dp0
 cd ../
-git clone git@github.com:okayumoka/ponkan.git ponkan
+git clone git@github.com:studiomikan/ponkan.git ponkan
 cd ./ponkan
 REM git checkout -b develop origin/develop
 @echo on
